6890e2e853ad1e7d032a22253eea6c3db6a03f9c
Vera Vlasova <vlasovavery@gmail.com>
2021-11-11 12:14:39 +0500

    display the help, the first task

---
+++ bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import program from 'commander';
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .option(' ')
+  .version('0.0.1')
+  .parse();
